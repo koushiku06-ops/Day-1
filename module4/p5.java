dcdde549e19f6e7629301216eddb0b246864f9d7
Koushik U <koushiku06@gmail.com>
2026-03-12 15:23:22 +0530

    Add files via upload

---
+++ module4/p5.java
@@ -0,0 +1,16 @@
+final class FinalClass {
+    final int value = 10; // final variable
+
+    final void display() { // final method
+        System.out.println("Value: " + value);
+    }
+}
+
+public class p5 {
+    public static void main(String[] args) {
+        FinalClass obj = new FinalClass();
+        obj.display();
+
+        // obj.value = 20; Error
+    }
+}
---
+++ module4/p6.java
@@ -0,0 +1,27 @@
+class Counter {
+
+    static int count = 0; // Static variable
+
+    Counter() {
+        count++;
+    }
+
+    static void showCount() { // Static method
+        System.out.println("Total Objects Created: " + count);
+    }
+}
+
+public class p6 {
+    public static void main(String[] args) {
+
+        Counter c1 = new Counter();
+        Counter.showCount();
+
+        Counter c2 = new Counter();
+        Counter.showCount();
+
+        Counter c3 = new Counter();
+        Counter.showCount();
+    }
+
+}
