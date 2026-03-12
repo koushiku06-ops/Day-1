40c217f3a5f75dc1ef6fcce9466419b44acf5362
Koushik U <koushiku06@gmail.com>
2026-03-12 11:27:45 +0530

    Add files via upload

---
+++ module2/stud.java
@@ -0,0 +1,35 @@
+class Student {
+
+    String name;
+    int age;
+
+    // Default Constructor
+    Student() {
+        name = "Default";
+        age = 0;
+    }
+
+    // Parameterized Constructor
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class stud {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();// Default constructor
+        Student s2 = new Student("koushik", 21);// Parameterized constructor
+
+        s1.display();
+        System.out.println();
+        s2.display();
+    }
+}
